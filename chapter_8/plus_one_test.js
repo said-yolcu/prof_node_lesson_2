de9e3e22179fb2a9c8f41fe83647e7d313a7770c
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2022-07-06 17:27:55 +0300

    Mind the relative addresses you write in your js files

--- chapter_8/plus_one_test.js
+++ chapter_8/plus_one_test.js
@@ -1,32 +1,44 @@
 //const { clearInterval } = require('timers')
 
 var spawn = require('child_process').spawn
-var child= spawn('node', ['plus_one.js'])
+var path = require('path')
+
+var path_to_js= path.relative('.', '/home/bitay/computer_science/\
+github_repositories/prof_node_lesson_2/chapter_8/plus_one.js')
+
+var child= spawn('node', [path_to_js])
+
 var print_out= setInterval(() => {
     var number= Math.floor(Math.random() * 10000)
-
+    
     child.stdin.write(number + '\n')
-
+    
     child.stdout.once('data', data => {
-        process.stdout.write(`child replied to  ${number} with:  ${data}\n`)
+        console.log(`child replied to  ${number} with:  ${data}\n`)
     })
     //process.stdout.write('child stdout\n')
-
+    
     child.stderr.on('data', data => {
         process.stdout.write(data)
     })
+    
 }, 1000)
 
 setTimeout(() => {
     clearInterval(print_out)
+    // Alt 1 => process.exit(0)
     child.kill()
-    // Alt 1 => 
+    //process.exit()
 }, 3500)
 
+// Alt 1 => process.on(exit, ...)
 child.on('exit', (code, signal) => {
     if (code){
         console.log(`Process to exit with code ${code}`)
     }else if (signal){
         console.log(`child process terminated because of signal ${signal}`)
     }
+    
 })
+
+// Alt 1 
